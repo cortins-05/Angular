80bf2be17bf0737ddbbb41aef2edc81e86b3b5b9
cortins-05 <lucasortins@gmail.com>
2025-11-15 21:57:40 +0100

    a

--- 06-reactive-forms-app/src/app/auth/pages/registrer-page/registrer-page.ts
+++ 06-reactive-forms-app/src/app/auth/pages/registrer-page/registrer-page.ts
@@ -1,9 +1,29 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-registrer-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe,ReactiveFormsModule],
   templateUrl: './registrer-page.html',
 })
-export class RegistrerPage { }
+export class RegistrerPage {
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+  myForm = this.fb.group({
+    name:['',[Validators.required, Validators.pattern(FormUtils.namePattern)]],
+    email:['',[Validators.required,Validators.email, Validators.pattern(FormUtils.emailPattern)],[FormUtils.checkingServerResponse]],
+    username: ['',[Validators.required, Validators.minLength(6), Validators.pattern(FormUtils.notOnlySpacesPattern)]],
+    password: ['',[Validators.required,Validators.minLength(6)]],
+    confirmPassword: ['',Validators.required]
+  },{
+    validators: [
+      FormUtils.isFieldOneEqualFieldTwo('password','confirmPassword')
+    ]
+  })
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
+}
--- 06-reactive-forms-app/src/app/utils/form-utils.ts
+++ 06-reactive-forms-app/src/app/utils/form-utils.ts
@@ -1,9 +1,25 @@
-import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+import { AbstractControl, FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+
+
+function sleep(){
+  return new Promise(resolve=>{
+    setTimeout(()=>{
+      resolve(true);
+    },2500)
+  })
+}
 
 export class FormUtils{
 
+  //Expresiones regulares
+  static namePattern = "([a-zA-Z]+) ([a-zA-Z]+)";
+  static emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+  static notOnlySpacesPattern = "^[a-zA-Z0-9]+$";
+
   //Errores regulares
   static getTextError(errors:ValidationErrors){
+    console.log(errors);
+
     for(const key of Object.keys(errors)){
       switch (key){
         case 'required':
@@ -12,6 +28,23 @@ export class FormUtils{
           return `Minimo de ${errors['minlength'].requiredLength} caracteres`
         case 'min':
           return `Valor minimo de ${errors['min'].min}`
+        case "email":
+          return "El valor ingresado no es un correo electronico";
+        case "emailTaken":
+          return "El correo electronico esta usado por otro usuario";
+        case "pattern":
+          if(errors['pattern'].requiredPattern==FormUtils.emailPattern){
+            return "El correo electronico no es permitido"
+          }
+          if(errors['pattern'].requiredPattern==FormUtils.namePattern){
+            return "El nombre no esta permitido"
+          }
+          if(errors['pattern'].requiredPattern.includes(FormUtils.notOnlySpacesPattern)){
+            return "Not only spaces pattern";
+          }
+          return "Error de patron contra expresion regular"
+        default:
+          return `Error de validacion no controlado ${key}`;
       }
     }
     return null
@@ -37,6 +70,29 @@ export class FormUtils{
     if(form.controls.length==0)return null;
     const errors = form.controls[index].errors ?? {};
     return FormUtils.getTextError(errors);
+  }
+
+  static isFieldOneEqualFieldTwo(field1:string,field2:string){
+    return (formGroup:AbstractControl) =>{
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+      return field1Value === field2Value ? null : {passwordsNotEqual:true} ;
+    }
+  }
+
+  static async checkingServerResponse(control:AbstractControl):Promise<ValidationErrors|null>{
+
+    await sleep();
+
+    const formValue = control.value;
+
+    if(formValue=="hola@mundo.com"){
+      return{
+        emailTaken:true
+      }
+    }
+
+    return null;
   }
 
 }
